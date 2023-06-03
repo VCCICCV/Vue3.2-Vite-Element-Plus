c4a594429a503dfa41023995be5f2710b81e24ea
VCCICCV <chaochen497@gmail.com>
2023-06-03 20:05:04 +0800

    布局优化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {ref} from "vue"
+import { ref } from "vue"
+
 // 数据
 let input = ref("")
 const tableData = ref([
@@ -24,9 +25,13 @@ const tableData = ref([
     address: 'No. 189, Grove St, Los Angeles',
   },
 ])
+let multipleSelection = ref([])
 // 方法
 const handleRowClick = () => {
   console.log('click')
+}
+const handleSelectionChange = (val) => {
+  multipleSelection.value = val
 }
 </script>
 
@@ -37,36 +42,52 @@ const handleRowClick = () => {
     </div>
     <!-- query -->
     <div class="query-box">
-      <el-input v-model="input" placeholder="请输入" />
+      <el-input v-model="input" placeholder="请输入姓名" />
       <el-button type="primary">Primary</el-button>
     </div>
     <!-- table -->
-    <el-table :data="tableData" style="width: 100%">
-    <el-table-column fixed prop="date" label="Date" width="150" />
-    <el-table-column prop="name" label="Name" width="120" />
-    <el-table-column prop="state" label="State" width="120" />
-    <el-table-column prop="city" label="City" width="120" />
-    <el-table-column prop="address" label="Address" width="600" />
-    <el-table-column prop="zip" label="Zip" width="120" />
-    <el-table-column fixed="right" label="Operations" width="120">
-      <template #default>
-        <el-button link type="primary" size="small" @click="handleRowClick"
-          >Detail</el-button
-        >
-        <el-button link type="primary" size="small">Edit</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+    <el-table :data="tableData" border style="width: 100%" ref="multipleTableRef"
+      @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" />
+      <el-table-column label="Date" width="120">
+        <template #default="scope">{{ scope.row.date }}</template>
+      </el-table-column>
+      <el-table-column fixed prop="date" label="Date" width="150" />
+      <el-table-column prop="name" label="Name" width="120" />
+      <el-table-column prop="state" label="State" width="120" />
+      <el-table-column prop="city" label="City" width="120" />
+      <el-table-column prop="address" label="Address" width="600" />
+      <el-table-column prop="zip" label="Zip" width="120" />
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template #default>
+          <el-button link type="primary" size="small" @click="handleRowClick">Detail</el-button>
+          <el-button link type="primary" size="small">Edit</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
-
 </template>
 
 <style scoped>
-.table-box{
-  width:800px;
+.table-box {
+  width: 800px;
   position: absolute;
-  top:50%;
+  top: 50%;
   left: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
+}
+
+.title {
+  text-align: center;
+}
+
+.query-box {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.el-input {
+  width: 200px;
 }
 </style>
